876d5abf11b9c2ddd788144e8962d14b38ce3233
elizabethshulman <ers58@duke.edu>
2018-09-02 11:39:19 -0400

    Generates initial FoodTracker app files (Closes #1)

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Elizabeth Shulman on 9/1/18.
+//  Copyright © 2018 Elizabeth Shulman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
