c0c3e5cad442e9397ee806b65028d1e5a72b3feb
elizabethshulman <ers58@duke.edu>
2018-09-03 03:23:18 -0400

    UI-04: Creates RatingControl custom view (Closes #6)

---
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Elizabeth Shulman on 9/3/18.
+//  Copyright © 2018 Elizabeth Shulman. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
