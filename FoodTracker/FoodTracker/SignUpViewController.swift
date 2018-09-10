822f2fd3c61a6f365d5186b80340a6105148710b
elizabethshulman <ers58@duke.edu>
2018-09-10 07:42:24 -0400

    OA-01: Adds views and Firebase integration to support login. Closes #29 and #30.

---
+++ FoodTracker/FoodTracker/SignUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignUpViewController.swift
+//  FoodTracker
+//
+//  Created by Elizabeth Shulman on 9/10/18.
+//  Copyright © 2018 Elizabeth Shulman. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var reenterPasswordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodTracker/FoodTracker/StartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StartViewController.swift
+//  FoodTracker
+//
+//  Created by Elizabeth Shulman on 9/10/18.
+//  Copyright © 2018 Elizabeth Shulman. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
